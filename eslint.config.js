5eb0b3130ca1abcc190284b573286d161c61be3a
Ben Wilson <ben.wilson@smartbear.com>
2025-06-20 15:48:29 +0100

    add comment

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ const customRules = {
   "no-unused-vars": "off",
   "no-redeclare": "off",
   "@typescript-eslint/no-unused-vars": "off",
-  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-explicit-any": "off", // (Explicit) any has its valid use cases
   "@typescript-eslint/triple-slash-reference": "off",
   "@typescript-eslint/no-namespace": "off",
   "@typescript-eslint/no-empty-object-type": "off"
