1bb444f29fb29197e43303ab640394e8329d76dc
Ben Wilson <ben.wilson@smartbear.com>
2025-06-20 12:21:47 +0100

    install and configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+/** @type {import("eslint").Linter.RulesRecord} */
+const customRules = {
+  "no-unused-vars": "off",
+  "@typescript-eslint/no-unused-vars": "off",
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/triple-slash-reference": "off",
+}
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], rules: customRules },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node }, rules: customRules },
+]);
