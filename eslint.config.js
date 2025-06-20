f355adeb467a598dbf25dbe6948930f3c0872f2d
Ben Wilson <ben@topwoodstudios.co.uk>
2025-06-20 17:21:33 +0100

    Update eslint.config.js
    Co-authored-by: Sachin Pande <sazap10@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -10,12 +10,14 @@ const customRules = {
 }
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    ignores: ["**/dist/**", "**/node_modules/**", "**/coverage/**"],
-    languageOptions: { globals: globals.node } 
+  {
+    ignores: ["dist/**", "**/node_modules/**", "**/coverage/**"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node }
   },
   tseslint.configs.recommended,
   { rules: customRules }
