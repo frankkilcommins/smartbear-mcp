ab6a0a862fc0962a887c8a411f0ce1fc86183ca9
Sachin Pande <sazap10@gmail.com>
2025-06-24 13:06:07 +0100

    use mcp server name config

--- common/info.ts
+++ common/info.ts
@@ -1,3 +1,3 @@
 import packageJson from '../package.json' with { type: "json" };
-export const MCP_SERVER_NAME = "SmartBear MCP Server";
+export const MCP_SERVER_NAME = packageJson.config.mcpServerName;
 export const MCP_SERVER_VERSION = packageJson.version;
